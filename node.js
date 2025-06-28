14da588ce48dbbfb19c24a1bfb1ddcd6c59d8273
gowsikprem <gowsikprem2005@gmail.com>
2025-06-28 15:43:22 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,11 @@
+const html=() =>{
+ alert("Thank you Enrolling HTML, CSS, JS")
+}
+
+const app =() =>{
+ alert("Thank you Enrolling Application Development")
+}
+
+const ml =() =>{
+ alert("Thank you Enrolling Machine Learning")
+}
